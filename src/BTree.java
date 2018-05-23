40b4ca338ce73cae5001dc3f2d30efd1671628dc
Liav <liavbapp@gmail.com>
2018-05-23 23:09:52 +0300

    dd

--- src/BTree.java
+++ src/BTree.java
@@ -2,16 +2,33 @@ public class BTree <T> {
 
     private BTreeNode<T> _root;
     Integer tValue;
+
     public BTree(String val) {
-        this.tValue=Integer.valueOf(val);
-        this._root=new BTreeNode<>(0,true,null,null,tValue);
-    }
-    public void insert(BTreeNode<T>node,int key) {
-
+        this.tValue = Integer.valueOf(val);
+        this._root = new BTreeNode<>(tValue, true);
     }
 
+    public void insert(T key) {
+        if(isFull()) {
+            BTreeNode<T> s=new BTreeNode<>(tValue,false);
+            _root=s;
+            s.set_keysNumber(0);
+            s.addChilds(0,_root);
+            s.splitChild(0);
+        }
+        else {
+            _root.insertNonFull(key);
+        }
 
+    }
 
-
-
+    public boolean search(T key) {
+        if (_root == null) //empty tree
+            return false; //not found
+        else
+           return _root.search(key); //let the root search for the key
+    }
+    private boolean isFull() {
+        return 2*tValue-1==_root.get_keysNumber();
+    }
 }
--- src/BTreeNode.java
+++ src/BTreeNode.java
@@ -6,15 +6,16 @@ public class BTreeNode <T> {
     private LinkedList<BTreeNode<T>> _childsList;
     int _tval;
 
-        public BTreeNode(int keysNum,boolean isLeaf,LinkedList<T> keys,LinkedList<BTreeNode<T> childs,int tval) {
-            this._keysNumber=keysNum;
+        public BTreeNode(int tval,boolean isLeaf) {
+            this._keysNumber=0;
             this._isLeaf=isLeaf;
             this._keysList =new LinkedList<>();
             this._childsList =new LinkedList<>();
             this._tval=tval;
         }
 
-    private void splitChild(int index) {
+
+    public void splitChild(int index) {
         BTreeNode<T> y= _childsList.get(index);
         BTreeNode<T> z=new BTreeNode<>(_tval-1,y.is_isLeaf(),null,null,_tval);
         //add keys to 'z' from key 't' to key '2t-1'
@@ -28,32 +29,86 @@ public class BTreeNode <T> {
             }
         }
         y.set_keysNumber(_tval-1);
-        //add new index to child list and keys list ( lists size increase by 1)
-        this._childsList.add(null);
-        this._keysList.add (null);
-        // moving children's list one step right to add new pointer to child 'z'
-        for(int j=this._keysNumber+1;j<=index+1;j=j-1) {
-            this.setChild(j,this._childsList.get(j-1));
-        }
-        this.setChild(index+1,z);
-        //moving keys one step right
-        for(int j=this._keysNumber;j<=index;j=j-1) {
-            this.setKey(j+1,_keysList.get(j));
-        }
-        //set the new key to this node (from 'y' list)
-        this.setKey(index,y.get_keysList().get(_tval));
+        //add new child to child list
+        this.addChilds(index+1,z);
+        this.addKey(index,y.get_keysList().get(_tval));
         this.set_keysNumber(this._keysNumber+1);
+
+        //add new index to child list and keys list ( lists size increase by 1)
+//        this._childsList.add(null);
+//        this._keysList.add (null);
+//        // moving children's list one step right to add new pointer to child 'z'
+//        for(int j=this._keysNumber+1;j<=index+1;j=j-1) {
+//            this.setChild(j,this._childsList.get(j-1));
+//        }
+//        this.setChild(index+1,z);
+//        //moving keys one step right
+//        for(int j=this._keysNumber;j<=index;j=j-1) {
+//            this.setKey(j+1,_keysList.get(j));
+//        }
+//        //set the new key to this node (from 'y' list)
+//        this.setKey(index,y.get_keysList().get(_tval));
+//        this.set_keysNumber(this._keysNumber+1);
 
 
     }
 
-    private void addKey(int index,T key) {
+
+    public void insertNonFull(T key) {
+
+        // Initialize index i as the most right element in the node
+        int i=_keysNumber-1;
+
+        if (_isLeaf) {
+            while(i>=0 && ((String)_keysArr.get(i)).compareTo(((String)key))<0) //finds the right place to insert the key
+                i--;
+
+            //Insert the new key at the location we've found
+            _keysArr.add(i+1,key);
+            _keysNumber++;
+        }
+        else { //the node isn't a leaf
+
+            //finds the child which will contain the new key
+            while(i>=0 && ((String)_keysArr.get(i)).compareTo(((String)key))<0)
+                i--;
+
+            //checks whether the child is full or not
+            if(_childsList.get(i+1).get_keysNumber()==2*_tval-1) {
+
+                //full indeed, split it please
+                _keysArr.get(i + 1).splitChild(i + 1);
+                if(((String)_keysArr.get(i+1)).compareTo(((String)key))>0)
+                    i++;
+            }
+            _childsList.get(i+1).insertNonFull(key);
+
+        }
+
+    }
+
+    public boolean search(T key) {
+        int i=0;
+        while(i<_keysNumber && ((String)_keysArr.get(i)).compareTo(((String)key))<0)
+            i++;
+        if(i<_keysNumber && ((String)key).equals(((String)_keysArr.get(i)))) //found
+            return true;
+        if(_isLeaf) //no children to search in
+            return false;
+        else
+            return _childsList.get(i).search(key);
+    }
+
+
+
+
+    public void addKey(int index,T key) {
             this._keysList.add(index,key);
     }
     private void setKey(int index,T key) {
             this._keysList.set(index,key);
     }
-    private void addChilds(int index,BTreeNode<T> child) {
+    public void addChilds(int index,BTreeNode<T> child) {
             this._childsList.add(index,child);
     }
     private void setChild(int index,BTreeNode<T>child) {
@@ -84,11 +139,11 @@ public class BTreeNode <T> {
         this._keysList = _keysList;
     }
 
-    public LinkedList<BTreeNode> get_childsList() {
+    public LinkedList<BTreeNode<T>> get_childsList() {
         return _childsList;
     }
 
-    public void set_childsList(LinkedList<BTreeNode> _childsList) {
+    public void set_childsList(LinkedList<BTreeNode<T>> _childsList) {
         this._childsList = _childsList;
     }
 
