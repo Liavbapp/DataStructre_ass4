df4483fc42ba4b9010c1fe2d8daa3d2f14137a7b
Liav <liavbapp@gmail.com>
2018-05-25 22:23:20 +0300

    changed to regular array

--- src/Messages.java
+++ src/Messages.java
@@ -6,46 +6,37 @@ import java.io.IOException;
 import java.util.Iterator;
 
 public class Messages  implements Iterable<Message> {
-    LinkedList<Message>messageList;
-    public Messages() {
-        messageList=new LinkedList<>();
-    }
+
+    Message[]messageArr;
+    Message messageObject;
+
+
     public void generateMessages(String MessagesPath) {
-        String line="";
-//        int counter=0;
+        String line = "";
+        messageArr=new Message[countMessages((MessagesPath))];
         BufferedReader br = null;
-        try {
-            br = new BufferedReader(new FileReader(MessagesPath));
+        int c=0;
+        try { br = new BufferedReader(new FileReader(MessagesPath));
             //first line is empty so we read it and move on
             br.readLine();
-            Message messageObject;
-            while ((line = br.readLine()) != null) {
-               messageObject =new Message();
-                while (line!=null&&!line.equals("#")) {
-                    if(getlineType(line)==1) {
+            while ((line = br.readLine()) != null) { messageObject = new Message();
+                while (line != null && !line.equals("#")) {
+                    if (getlineType(line) == 1) {
                         messageObject.setSenderName(line.substring(5));
-                        line=br.readLine();
-                    }
-                    else if(getlineType(line)==2) {
+                        line = br.readLine();
+                    } else if (getlineType(line) == 2) {
                         messageObject.setReciverName(line.substring(3));
-                        line=br.readLine();
-                    }
-                    else {
-                        String message=line;
-                        while ((line = br.readLine())!=null&&!line.equals("#")) {
-                            message=message+line;
-                        }
+                        line = br.readLine();
+                    } else {
+                        String message = line;
+                        while ((line = br.readLine()) != null && !line.equals("#")) message = message + line;
                         messageObject.setMessage(message);
-                        messageList.add(messageObject);
-
-                        }
+                        messageArr[c]=messageObject;
+                        c=c+1;
+                    }
                 }
             }
-//            if(counter==0) {
-//                throw new IllegalArgumentException("the txt file is empty");
-//            }
-        }
-        catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
@@ -62,34 +53,55 @@ public class Messages  implements Iterable<Message> {
 
     // 1.sender 2.reciver 3.message
     private int getlineType(String line) {
-        int linelength=line.length();
+        int linelength = line.length();
 
-        if(linelength==0) {
-            throw new IllegalArgumentException("empty message");
-        }
-        else if (linelength==1|linelength==2) {
+        if (linelength == 0) {
+            // if line is empty we throw an exception
+            throw new IllegalArgumentException("empty line in a message");
+        } else if (linelength == 1 | linelength == 2) {
             return 3;
         }
-        if(line.substring(0,3).equals("To:")) {
+        if (line.substring(0, 3).equals("To:")) {
             return 2;
-        }
-        else if (line.substring(0,5).equals("From:")) {
+        } else if (line.substring(0, 5).equals("From:")) {
             return 1;
+        } else return 3;
+    }
+
+    //function retrive the number of messages that will be in the txt file
+    private int countMessages(String path) {
+        int counter = 1;
+        String line = "";
+        BufferedReader br = null;
+        boolean isFileEmpty = true;
+        try {
+            br = new BufferedReader(new FileReader(path));
+            //first line is empty so we read it and move on
+            br.readLine();
+            while ((line = br.readLine()) != null) {
+                //check validity of the line, if there it is an empty line error will thrown
+                getlineType(line);
+                if (isFileEmpty) isFileEmpty = false;
+                if (line.equals("#")) counter = counter + 1;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-        else return 3;
-
-    }
-    public boolean add(Message msg) {
-        return messageList.add(msg);
-    }
-    public int size() {
-        return messageList.size();
-    }
-    public Message getAtIndex(int index)
-    {
-        return messageList.get(index);
+    return counter;
     }
 
+    public int size(){ return messageArr.length;}
+    public Message getAtIndex(int index) {return messageArr[index];}
     @Override
         public Iterator<Message> iterator() {
       return  new MessageListIterator(this);
